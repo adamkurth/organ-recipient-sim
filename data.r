13d029bd3eb0ba50dba5d9c9ec8e3e4c46c12f19
adamkurth <k33go75@gmail.com>
2024-10-08 19:06:21 -0700

    data.r/selection.r

--- data.r
+++ data.r
@@ -1,24 +1,90 @@
 rm(list=ls())
+cwd <- getwd()
 
-d <- 10 # donors
-r <- 10 # recipients
+# Parameters
+d <- 10  # number of donors
+r <- 10  # number of recipients
+k <- 3   # number of features
 
-# generate random data
-set.seed(1)
-k <- 3 # features of interest (e.g. age, blood type, etc.)
+# Constraints
+age.min <- 2
+age.max <- 80
 
-data.d <- data.frame(
-  donor = 1:d,
-  value = rnorm(d)
-)
-data.r <- data.frame(
-    recipient = 1:r,
-    eval = rnorm(r) #
-)
+# Helper functions
+generate.blood.type <- function(n) {
+  blood.types <- c("A", "B", "AB", "O")
+  blood.type.prob <- c(0.3, 0.2, 0.1, 0.4)
+  sample(blood.types, n, replace=TRUE, prob=blood.type.prob)
+}
 
+generate.age <- function(n, is.donor) {
+  if(is.donor) {
+    # Donors are more likely to be younger
+    alpha <- 2
+    beta <- 3
+    round(rbeta(n, shape1 = alpha, shape2 = beta) * (age.max - age.min) + age.min)
+  } else {
+    # Recipients have a more uniform distribution, slightly skewed towards older ages
+    alpha <- 2
+    beta <- 1.5
+    round(rbeta(n, shape1 = alpha, shape2 = beta) * (age.max - age.min) + age.min)
+  }
 
-data.r <- data.frame(
-  donor = sample(1:d, 100, replace = TRUE),
-  recipient = sample(1:r, 100, replace = TRUE),
-  value = rnorm(100)
-)
+}
+# hist(generate.age(10000, TRUE), breaks=seq(0, 100, by=5), main="Donor Age Distribution", xlab="Age", ylab="Frequency")
+# hist(generate.age(10000, FALSE), breaks=seq(0, 100, by=5), main="Donor Age Distribution", xlab="Age", ylab="Frequency")
+
+generate.health.score <- function(age, is.donor) {
+  # Normalize age to a 0-1 scale
+  age.normalized <- (age - age.min) / (age.max - age.min)
+  
+  # Create a non-linear age weighting effect (more pronounced for older ages)
+  age.effect <- 1 - age.normalized^2 # Quadratic effect
+  
+  # Base score ranges
+  # donor: [0.6, 1.0], recipient: [0.1, 0.8]
+  base.score.min <- if(is.donor) 0.6 else 0.1
+  base.score.max <- if(is.donor) 2.0 else 0.8
+  
+  # Generate base score
+  base.score <- runif(1, base.score.min, base.score.max)
+  
+  # Combine base score with age effect
+  health.score <- base.score * age.effect
+  
+  # Ensure health score is within [0, 1] range
+  health.score <- pmax(0, pmin(1, health.score))
+  
+  return(health.score)
+}
+
+# Generate data for donors
+generate.donor.data <- function(n) {
+  ages <- generate.age(n, is.donor=TRUE)
+  data.frame(
+    id = paste0("D", 1:n),
+    age = ages,
+    blood.type = generate.blood.type(n),
+    health.score = sapply(ages, generate.health.score, is.donor=TRUE),
+    time = sample(1:24, n, replace=TRUE)  # Time available in hours
+  )
+}
+
+# Generate data for recipients
+generate.recipient.data <- function(n) {
+  ages <- generate.age(n, is.donor=FALSE)
+  data.frame(
+    id = paste0("R", 1:n),
+    age = ages,
+    blood.type = generate.blood.type(n),
+    health.score = sapply(ages, generate.health.score, is.donor=FALSE),
+    time = sample(1:24, n, replace=TRUE)  # Time listed in hours
+  )
+}
+
+# Generate data
+data.d <- generate.donor.data(d)
+data.r <- generate.recipient.data(r)
+
+# Save data
+save(data.d, data.r, file=paste0(cwd,"data.RData"))
---
+++ selection.r
@@ -0,0 +1,83 @@
+rm(list=ls())
+load("data.RData")
+
+comp.matrix <- matrix(FALSE, nrow = nrow(data.r), ncol = nrow(data.d))
+rownames(comp.matrix) <- data.r$id
+colnames(comp.matrix) <- data.d$id
+
+# initial filtering based on blood compatible type
+is.same.blood.type <- function(donor, recipient){
+    compatible <- list(
+        "O" = c("A", "B", "AB", "O"),
+        "A" = c("A", "AB"),
+        "B" = c("B", "AB"),
+        "AB" = c("AB")
+    )
+    return(recipient %in% compatible[[donor]])
+}
+
+# filter based on blood type
+for (i in 1:nrow(data.r)) {
+    for (j in 1:nrow(data.d)) {
+        comp.matrix[i, j] <- is.same.blood.type(data.d$blood.type[j], data.r$blood.type[i])
+    }
+}
+
+sim.doctor.eval <- function(donor, recipient){
+    # age difference (normalized)
+    age.diff <- abs(donor$age - recipient$age) / 80 # max age difference is 80
+
+    # health score difference (considering donor should be healthier than recipient)
+    health.diff <- max(0, donor$health.score - recipient$health.score)
+
+    # time factor (preference for longer-waiting recipients)
+    # recipient$time in 24 hour period for a given day
+    time.factor <- min(1, recipient$time.listed / 24) # IMPLEMENT TOTAL ELAPSED TIME DIVISION instead of /24
+
+    approval.prob <- (
+        0.4 * (1 - age.diff) +  # Age difference contributes 40%
+        0.3 * donor$health.score +  # Donor health contributes 30%
+        0.25 * health.diff +  # Health difference contributes 25%
+        0.05 * time.factor  # Time factor contributes 5%
+    )
+
+    # Add some randomness to represent individual doctor's judgment
+    approval.prob <- pmax(0, pmin(1, approval.prob + rnorm(1, mean = 0, sd = 0.1)))
+
+    return(runif(1) < approval.prob)
+}
+
+num.doctors <- 5 
+doctor.evals <- array(FALSE, dim = c(nrow(data.r), nrow(data.d), num.doctors))
+
+# For each donor, evaluate only compatible recipients
+for (j in 1:nrow(data.d)) { # for each donor
+    comp.recipients <- which(comp.matrix[, j]) # get compatible recipients
+    for (i in comp.recipients) { 
+        for (k in 1:num.doctors) { 
+            doctor.evals[i, j, k] <- sim.doctor.eval(data.d[j,], data.r[i,]) # evaluate each recipient by each doctor for the donor
+        }
+    }
+}
+
+# Function to get doctor consensus for a specific donor
+get.consensus <- function(donor.idx) {
+    comp.recipients <- which(comp.matrix[, donor.idx])
+    consensus <- rep(FALSE, length(comp.recipients))
+    for (i in seq_along(comp.recipients)) {
+        recipient_index <- comp.recipients[i]
+        approvals <- sum(doctor.evals[recipient_index, donor.idx, ])
+        consensus[i] <- approvals > num.doctors/2
+    }
+    return(data.frame(
+        recipient.id = data.r$id[comp.recipients],
+        consensus = consensus,
+        approvals = apply(doctor.evals[comp.recipients, donor.idx, ], 1, sum)
+    ))
+}
+
+
+# Print results for each donor\
+donor.sample <- sample(1:nrow(data.d), 1, replace=FALSE)
+print(data.d[donor.sample,])
+get.consensus(donor.sample)
